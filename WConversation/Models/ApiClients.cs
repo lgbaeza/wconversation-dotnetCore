22bd93aa99d70337e1460ca6de7890ed8b85bc71
Luis Gerardo Baeza Castro <lgbaeza@mx1.ibm.com>
2018-02-14 08:31:23 -0600

    api client db

--- WConversation/Models/ApiClients.cs
+++ WConversation/Models/ApiClients.cs
@@ -3,11 +3,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data.MySqlClient;
+
 
 namespace WatsonSamples.Models
 {
     public class ApiClients
     {
+        public static int GetProductPrice(string product, int users, string planType)
+        {
+            var res = 0;
+
+            string server = "sl-us-south-1-portal.20.dblayer.com",
+            port = "34087",
+            database = "demo_conta",
+            uid = "demo_conta",
+            password = "demo_conta";
+            string connectionString = "SERVER=" + server + "; port = " + port + ";DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            var connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT price from products where productname = '" + product + "' and producttype = '" + planType + "' and users = " + users;
+
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                connection.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    
+                }
+            }
+
+            return res;
+        }
+
         public static string ValidateRFC(string rfc)
         {
             //Initialize restsharp client
